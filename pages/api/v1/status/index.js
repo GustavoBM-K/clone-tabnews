07d391dcddd42638bb6d60ea6766cdd786f1bae5
Gustavo Berti <gu_cju@hotmail.com>
2025-11-24 20:39:57 -0300

    add database.js and .env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 90/10 AS result;");
+  console.log("Request at", new Date(), request.headers["user-agent"]);
+  console.log(result.rows);
   response.status(200).json({ status: "Estão funcionando!" });
 }
 
