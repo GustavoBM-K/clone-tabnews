138679ec816a886ba0dc1490ad307998162f85c4
Gustavo Berti <gu_cju@hotmail.com>
2026-01-20 19:49:48 -0300

    add rejectUnauthorized:false to database.js connection for a aiven host service test

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: { rejectUnauthorized: false },
   });
   console.log("Credentials:", {
     host: process.env.POSTGRES_HOST,
