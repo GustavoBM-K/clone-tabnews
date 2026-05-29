8bdf84e2a19cb41ebd28392dec153d46665927d8
Gustavo Berti <gu_cju@hotmail.com>
2026-05-29 18:48:45 -0300

    fix prettier style linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,20 +1,18 @@
-const { exec } = require("node:child_process")
+const { exec } = require("node:child_process");
 
-function checkPostgres(){
-  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn)
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
-  function handleReturn(error, stdout){
-    if (stdout.search("accepting connections") === -1){
-      process.stdout.write(".")
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
       checkPostgres();
       return;
-    };
+    }
 
-    console.log("\nPostgres está pronto e aceitando conexões!\n")
-
+    console.log("\nPostgres está pronto e aceitando conexões!\n");
   }
 }
 
-
-      process.stdout.write("\n\n\Aguardando postgres aceitar conexões.")
-checkPostgres()
+process.stdout.write("\n\n\Aguardando postgres aceitar conexões.");
+checkPostgres();
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,20 +5,20 @@ async function waitForAllServices() {
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
-        retries: 100,
-        maxTimeout: 5000,
-        minTimeout: 1000,
-      });
+      retries: 100,
+      maxTimeout: 5000,
+      minTimeout: 1000,
+    });
 
     async function fetchStatusPage() {
-      const response = await fetch("http://localhost:3000/api/v1/status")
-      if (response.status != 200){
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      if (response.status != 200) {
         throw Error();
       }
     }
   }
 }
 
-export default{
+export default {
   waitForAllServices,
-} 
+};
